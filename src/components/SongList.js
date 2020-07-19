da039fb4078ab2cec8e51931e9812f147b6e9d3f
Awtanabe <panser098@gmail.com>
2020-07-19 23:51:24 +0900

    actioncreateを利用する

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {selectSong} from '../actions'
 
 class SongList extends Component {
   renderList() {
@@ -7,7 +8,10 @@ class SongList extends Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">
+            <button 
+              className="ui button primary" 
+              onClick={() => this.props.selectSong}
+            >
               Select
             </button>
           </div>
@@ -30,4 +34,8 @@ const mapStateToProps = (state) => {
   return {songs: state.songs}
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {
+  selectSong: selectSong
+})(SongList);
+// この書き方もok
+// export default connect(mapStateToProps, {selectSong})(SongList);
