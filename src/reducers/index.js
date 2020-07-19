5928512f49399013b21b5a3013577165f73ba8a6
Awtanabe <panser098@gmail.com>
2020-07-19 23:14:09 +0900

    reducer作成

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+// 初期データ
+const songsReducer = () => {
+  return [
+    {title: 'No Scrubs', duration: '4:05'},
+    {title: 'Macarena', duration: '2:30'},
+    {title: 'All star', duration: '3:15'}
+  ]
+}
+
+// Reducer
+const selectedSOngReducer = (selectedSong=null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+  return selectedSong
+}
