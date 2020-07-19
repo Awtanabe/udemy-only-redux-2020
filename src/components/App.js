3b9220b4fddcc770511af3774647a2c3f7e03895
Awtanabe <panser098@gmail.com>
2020-07-19 23:38:02 +0900

    connectでstoreのデータをつなぐ

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 // ネームドexport
 import { selectSong } from '../actions'
+import SongList from './SongList'
 
 // データはstoreに格納するから関数コンポーネントでいいよね
 function App() {
   return (
     <div className="App">
-      App
+      <SongList/>
     </div>
   );
 }
